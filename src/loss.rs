4bbf0ca25e157c46184d367461b1b9564322e740
Andrew Poulton <andy@Andrews-MacBook-Pro-2.local>
2020-10-26 19:38:27 +0400

    add untested BCE loss fn

--- src/loss.rs
+++ src/loss.rs
@@ -28,3 +28,29 @@ pub fn mse_loss(predictions:&Tensor, target:&Tensor)->Result<Loss, TensorError>{
         }
     }
 }
+#[inline]
+fn log_and_clamp(x: f32, min: f32, max: f32)->f32{
+    let out = x.ln();
+    if out > max {max} else if out < min {min} else {out}
+}
+
+pub fn bce_loss(predictions:&Tensor, target:&Tensor)-> Result<Loss, TensorError>{
+    match (predictions.tensor_type, target.tensor_type) {
+        (TensorType::Matrix, TensorType::Matrix) => {
+            Err(TensorError::ShapeError)
+        },
+        _ => {
+            let batch_size = predictions.data.len() as f32;
+            let loss = predictions.data.iter()
+                .zip(target.data.iter())
+                .map(|(x,y)| y*log_and_clamp(*x,-100.0f32, 100.0f32) + (1.0-y)*log_and_clamp(1.0-x,-100.0, 100.0))
+                .sum::<f32>();
+            let grad: Vec<f32> = predictions.data.iter()
+                .zip(target.data.iter())
+                .map(|(x,y)| (x-y)/(batch_size*x*(1.0-x)))
+                .collect();
+            
+            Ok(Loss { loss: -loss/ batch_size, loss_grad : Tensor::from_vec(grad).unwrap() })
+        }
+    }
+}
